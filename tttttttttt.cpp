575f18564e501e90f559137e2dd3ecd9573f1fb4
caowushang <15630929347@163.com>
2016-11-29 22:54:19 +0800

    aaaa

---
+++ tttttttttt.cpp
@@ -0,0 +1,17 @@
+/* ***********************************************
+Author        :caowushang
+Created Time  :2016年11月29日 星期二 22时51分55秒
+File Name     :tttttttttt.cpp
+************************************************ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+	cout << "fdsafdas" << endl;    
+    return 0;
+}
